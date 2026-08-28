86f327882c8ebc3e2241275fa98b54e0de70ec8d
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-08-28 11:19:22 -0400

    feat(app): criando função para exibir o menu interativo e retonar uma opção

--- src/app.py
+++ src/app.py
@@ -1,8 +1,36 @@
+
 def ler_opcao_numerica():
 
     while True:
         try:
             return int(input('Escolha uma das opções: '))
         except ValueError:
-            print('[ERRO]: Valor digitado inválido')
+            print('\n[ERRO]: Valor digitado inválido\n')
             continue
+
+def menu_interativo():
+
+    opcoes = [
+        'Population',
+        'Langue',
+        'Capitale',
+        'Culture',
+        'Gastronomie',
+        'Monuments',
+        'Quitter'
+    ]
+
+    for indice, opcao in enumerate(opcoes, start=1):
+        print('{} - {}'.format(indice, opcao))
+
+    while True:
+
+        alternativa = ler_opcao_numerica()
+
+        if (alternativa < 1 
+        or alternativa > 7):
+            print('\n[ERRO]: Escolha uma alternativa dentre as exibidas.\n')
+            continue
+        break
+
+    return alternativa
