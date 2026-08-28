0dc3b13969f86dc7158008b98d266641ae4d6b98
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-08-28 15:44:10 -0400

    feat(app): implemeando funcionalidade 1.
    - chore(ui): colocando um print de qual alternativa foi selecionada no menu_interativo para melhor visualização do usuário
    
    - refactor(app): colocando a limpeza de terminal dentro do fluxo while true para manter o terminal limpo

--- src/app.py
+++ src/app.py
@@ -43,30 +43,52 @@ def menu_interativo():
             continue
         break
 
+    print(f'Alternativa [{alternativa}] selecionada.')
     return alternativa
 
 def main():
 
     limpar_terminal()
     motivo_do_projeto()
-    limpar_terminal()
-    
-    match menu_interativo():
+    while True:
 
-        case 1:
-            pass
-        case 2:
-            pass
-        case 3:
-            pass
-        case 4:
-            pass
-        case 5:
-            pass
-        case 6:
-            pass
-        case 7:
-            pass
+        limpar_terminal()
+        match menu_interativo():
+
+            case 1:
+                print(DIVIDER)
+                _temp_stack = """ La France compte actuellement 
+    plus de 68 millions d'habitants répartis sur son 
+    territoire métropolitain et ses 
+    départements d'outre-mer. 
+
+    Cette population se caractérise par une grande diversité 
+    culturelle et une espérance de vie parmi les plus 
+    élevées au monde.
+
+    Les zones urbaines, notamment autour de Paris, 
+    Lyon et Marseille, concentrent une part 
+    importante des habitants.
+
+    Malgré un vieillissement démographique progressif, 
+    le pays maintient une dynamique de croissance 
+    grâce à un solde naturel et migratoire positif. """
+
+                POPULATION = "\n".join(l.center(57) for l in _temp_stack.splitlines())
+                print(POPULATION)
+                print(DIVIDER)
+            case 2:
+                pass
+            case 3:
+                pass
+            case 4:
+                pass
+            case 5:
+                pass
+            case 6:
+                pass
+            case 7:
+                pass
 
 if __name__ == '__main__':
     main()
