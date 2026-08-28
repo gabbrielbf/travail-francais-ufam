c5679595facbbbbb2b898eabf9095287b091cda5
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-08-28 11:32:51 -0400

    feat(lessons): criando mensagem personalizada do motivo no qual decidi fazer o projeto

--- src/lessons.py
+++ src/lessons.py
@@ -3,13 +3,31 @@ DIVIDER = '=' * 60
 
 def titulo():
 
-    _temp_stack = """========== 🇫🇷 LA FRANCE 🇫🇷 =========="""
-    BANNER = "\n".join(l.center(57) for l in _temp_stack.splitlines())
+    _temp_stack = """ ========== 🇫🇷 LA FRANCE 🇫🇷 ========== """
+    TITULO = "\n".join(l.center(57) for l in _temp_stack.splitlines())
 
     print(DIVIDER)
         
-    for char in BANNER: 
+    for char in TITULO: 
         sys.stdout.write(char)
         sys.stdout.flush()
         time.sleep(0.02)
     print()
+
+    print(DIVIDER)
+
+def motivo_do_projeto():
+
+    _temp_stack = """ J'ai utilisé mes connaissences 
+    en programmation pour créer un petit programme 
+    sur la France. """
+    MOTIVO = "\n".join(l.center(57) for l in _temp_stack.splitlines())
+
+    print(DIVIDER)
+        
+    for char in MOTIVO: 
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.02)
+    print()
+    
